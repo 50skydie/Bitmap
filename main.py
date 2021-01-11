fd3be9e0ad3aebfb221becdfd436f43e3f13d35a
dieplsky <dieplsky@gmail.com>
2021-01-11 09:39:55 +0100

    removed reading from .txt

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
 import bitmap as bm
 
-f = open('bitmap.txt', "r")
-a = []
-for x in f:
-     a.append(x.strip().rstrip().split(' '))
-
-#print(a)
 m1 = bm.BitMap([0])
 m1.readBitmap('bitmap.jpg')
 m1.scale(1)
